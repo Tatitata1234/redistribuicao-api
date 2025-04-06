4fa150fe29e3201f5de4f5cd8b1f10901bef8ec7
unknown <thais.s.land@gmail.com>
2025-04-06 09:43:24 -0300

    removendo colunas desnecessárias

--- create.sql
+++ create.sql
@@ -38,10 +38,8 @@ CREATE TABLE caixinha (
     nome VARCHAR(255) NOT NULL,
     valor_total NUMERIC(8, 2) NOT NULL,      -- Valor máximo 1.000.000 com 2 casas decimais
     valor_arrecadado NUMERIC(8, 2) NOT NULL, -- Valor máximo 1.000.000 com 2 casas decimais
-    investimento NUMERIC(8, 2) default 0,     -- Valor máximo 1.000.000 com 2 casas decimais
     id_classificacao BIGINT REFERENCES classificacao(id) ON DELETE SET NULL,
     id_utilidade BIGINT REFERENCES utilidade(id) ON DELETE SET NULL,
-    mensagem VARCHAR(500),  -- Campo de mensagem alterado para VARCHAR
     quitada BOOLEAN NOT NULL DEFAULT FALSE,
     id_usuario BIGINT REFERENCES usuario(id) ON DELETE CASCADE,
     ativo BOOLEAN NOT NULL DEFAULT TRUE
